9a7c6add9c1496103f723620ac024a3b742a1d69
devitalo97 <dev.italo.souza@gmail.com>
2025-03-10 10:48:35 -0300

    style: modify image

--- app/page.tsx
+++ app/page.tsx
@@ -311,7 +311,7 @@ export default function Example() {
                 <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                   <img
                     alt="App screenshot"
-                    src="https://tailwindcss.com/plus-assets/img/component-images/project-app-screenshot.png"
+                    src="/MOCK-LAPTOP.png"
                     width={2432}
                     height={1442}
                     className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
